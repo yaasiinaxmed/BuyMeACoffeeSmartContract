dce82e67cbbc72e7467f5208310d8dc24fb3303d
yaasiinaxmed <yaskassoy@gmail.com>
2024-11-27 20:27:33 +0300

    initial commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract BuyMeACoffee {
+    struct Coffee {
+        address sender;
+        string message;
+        uint256 timestamp;
+    }
+
+    uint256 totalCoffee;
+    address payable owner;
+
+    event newCoffee(address indexed, string message, uint256 timestamp);
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buyMeAcoffe(string memory _message) public payable {
+        require(msg.value > 0, "Value must be greater than 0");
+
+        totalCoffee += 1;
+
+        payable(owner).transfer(msg.value);
+
+        emit newCoffee(msg.sender, _message, block.timestamp);
+    }
+
+    function getTotalCoffee() public view returns (uint256) {
+        return totalCoffee;
+    }
 }
